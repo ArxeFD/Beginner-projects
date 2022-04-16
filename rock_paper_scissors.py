5613c66c0cbe900d94ab5f0d8b3412b99e81bc3b
ArxeFD <arsenny.filipov@gmail.com>
2022-04-16 10:08:34 -0600

    shortening the code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -11,46 +11,30 @@ else:
     print("Goodbye then...")
     quit()
 
-sleep(1.2)
+sleep(0.8)
 print("You'll have 3 tries!")
 
 points = 0
 tries = 0
 while tries < 3:
     variable = random.choice(list)
-    sleep(1.0)
+    sleep(0.8)
     game = input("rock paper or scissors? ").lower()
     print("Computers choice is", variable)
+    tries += 1
 
-    if variable == "scissors" and game == "scissors":
-        print("It's a draw!")
-        tries += 1
-    elif variable == "scissors" and game == "paper":
-        print("You lost!")
-        tries += 1
-    elif variable == "scissors" and game == "rock":
+    if game == "rock" and variable == "scissors":
         print("You won!")
-        tries += 1
         points += 1
-    elif variable == "rock" and game == "scissors":
+    elif game == "scissors" and variable == "paper":
         print("You won!")
-        tries += 1
         points += 1
-    elif variable == "rock" and game == "paper":
-        print("You lost!")
-        tries += 1
-    elif variable == "rock" and game == "rock":
-        print("It's a draw!")
-        tries += 1
-    elif variable == "paper" and game == "scissors":
-        print("You lost!")
-        tries += 1
-    elif variable == "paper" and game == "paper":
-        print("It's a draw!")
-        tries += 1
-    elif variable == "paper" and game == "rock":
+    elif game == "paper" and variable == "rock":
         print("You won!")
-        tries += 1
         points += 1
+    elif game == variable:
+        print("Draw!")
+    else:
+        print("You lose!")
 
 print("Your score is", points, "out of 3")
